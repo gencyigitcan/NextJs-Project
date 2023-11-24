2d89516131db39bec7e22cdff7be7bc03e38a802
Yiğitcan Genç <121306472+gencyigitcan@users.noreply.github.com>
2023-11-24 22:30:46 +0300

    NextJs Project Fourth Day

--- components/BlogCard/index.js
+++ components/BlogCard/index.js
@@ -2,18 +2,19 @@ import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 
-function BlogCard() {
+function BlogCard({title, body, id}) {
   return (
     <Link href="/" className={styles.card}>
         <div className={styles.cardImage}>
-            <Image src="https://picsum.photos/200/300" 
+            <Image 
+            src={`https://picsum.photos/200/300?random=${id}`}
             alt='Blog Card Image' 
             fill
             />
         </div>
         <div className={styles.cardBody}>
-           <h3>Title</h3>
-            <p>Lorem Ipsum</p>
+           <h3>{title}</h3>
+            <p>{body}</p>
         </div>
     </Link>
   )
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,22 @@
-import BlogCard from "../../components/BlogCard"
-import styles from "./styles.module.css"
+import BlogCard from "../../components/BlogCard";
+import styles from "./styles.module.css";
 
-export default function Home() {
+async function getPosts(){
+const response = await fetch("https://dummyjson.com/posts?limit=10");
+return response.json();
+}
+
+export default async function Home() {
+const { posts } = await getPosts();
+
   return (
-    <div>
-      <BlogCard/>
+    <div className={styles.blogsContainer}> 
+      {
+        posts.map(post=> 
+          <BlogCard key={post.id} {...post} /> 
+          ) 
+      }
+      
     </div>
   )
 }
