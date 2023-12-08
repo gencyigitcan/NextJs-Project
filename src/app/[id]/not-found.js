9a0bf92e43f9c41cadb71e76e494d1171265133c
Yiğitcan Genç <121306472+gencyigitcan@users.noreply.github.com>
2023-12-08 20:28:38 +0300

    NextJs Project Seventh Day

---
+++ src/app/[id]/not-found.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function notFound() {
+    return (
+        <div>
+            <h1>Blog doesn't exist</h1>
+        </div>
+    )
+}
+
--- src/app/[id]/page.js
+++ src/app/[id]/page.js
@@ -1,16 +1,26 @@
+import { notFound } from "next/navigation";
 import React from 'react';
 import Image from "next/image";
 
 import styles from "./styles.module.css";
 
 async function getPost(id){
+  try {
     const response = await fetch("https://dummyjson.com/posts/${id}");
     return response.json();
+      } 
+      catch(error) {
+        throw new Error(error);
+      }
 }
 
 export default async function Page({ params }) {
     const { id, title, body } = await getPost(params.id);
   
+    if(!title) {
+      return notFound();
+    }
+    
     return (
       <div className={styles.blogContainer}>
         <div className={styles.cardImage}>
